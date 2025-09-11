e06e26309b1b83b3c184169699ea9b6ecf0e1eda
giuseppeesposito22 <gius.espo22@gmail.com>
2025-09-11 15:59:54 +0200

    add method edit and update

--- src/main/java/org/lessons/java/spring_pizzeria/controller/DealController.java
+++ src/main/java/org/lessons/java/spring_pizzeria/controller/DealController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -24,12 +26,31 @@ public class DealController {
 
 
         if (bindingResult.hasErrors()) {
-            return "deals/create";
+            return "deals/create-or-edit";
         }
 
         dealRepository.save(deal);
 
         return "redirect:/pizzas/" + deal.getPizza().getId();
     }    
+
+    @GetMapping("/edit/{id}")
+    public String edit(Model model, @PathVariable Integer id){
+        model.addAttribute("deal", dealRepository.findById(id).get());
+        model.addAttribute("edit", true);
+
+        return"deals/create-or-edit";
+    }
+
+    @PostMapping("/update/{id}")
+    public String update(@Valid @ModelAttribute("deal") Deal deal, BindingResult bindingResult, Model model){
+        
+        if (bindingResult.hasErrors()) {
+            return"deals/create-or-edit";
+        }
+
+        dealRepository.save(deal);
+        return"redirect:/pizzas/" + deal.getPizza().getId();
+    }
 
 }
