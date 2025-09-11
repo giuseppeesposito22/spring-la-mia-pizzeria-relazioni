68e45e6dcc4b2ad29b340ea030a23e984d2c1617
giuseppeesposito22 <gius.espo22@gmail.com>
2025-09-11 13:50:22 +0200

    add deal controller

---
+++ src/main/java/org/lessons/java/spring_pizzeria/controller/DealController.java
@@ -0,0 +1,35 @@
+package org.lessons.java.spring_pizzeria.controller;
+
+import org.lessons.java.spring_pizzeria.model.Deal;
+import org.lessons.java.spring_pizzeria.repository.DealRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
+
+@Controller
+@RequestMapping("/deals")
+public class DealController {
+
+    @Autowired
+    private DealRepository dealRepository;
+
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("deal") Deal deal, BindingResult bindingResult, Model model){
+
+
+        if (bindingResult.hasErrors()) {
+            return "deals/create";
+        }
+
+        dealRepository.save(deal);
+
+        return "redirect:/pizzas/" + deal.getPizza().getId();
+    }    
+
+}
