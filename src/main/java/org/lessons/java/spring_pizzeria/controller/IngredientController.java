007abf4cf15a6589491c168673be992c86786ede
giuseppeesposito22 <gius.espo22@gmail.com>
2025-09-15 16:51:18 +0200

    add method create ingredients

--- src/main/java/org/lessons/java/spring_pizzeria/controller/IngredientController.java
+++ src/main/java/org/lessons/java/spring_pizzeria/controller/IngredientController.java
@@ -1,11 +1,18 @@
 package org.lessons.java.spring_pizzeria.controller;
 
+import org.lessons.java.spring_pizzeria.model.Ingredient;
 import org.lessons.java.spring_pizzeria.repository.IngredientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/ingredients")
@@ -18,6 +25,27 @@ public class IngredientController {
     public String index(Model model){
         model.addAttribute("ingredients", ingredientRepository.findAll());
         return "ingredients/index";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model){
+
+        model.addAttribute("ingredient", new Ingredient());
+        return"ingredients/create-or-edit";
+    }
+
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("ingredient") Ingredient ingredient, 
+    BindingResult bindingResult, 
+    Model model){
+
+        if(bindingResult.hasErrors()){
+            return "ingredients/create-or-edit";
+        }
+
+        ingredientRepository.save(ingredient);
+
+        return"redirect:/ingredients";
     }
 
 }
