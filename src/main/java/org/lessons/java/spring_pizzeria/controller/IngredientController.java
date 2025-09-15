7bf3cc84a102c34afe6f5fd4c760b3748cfadb05
giuseppeesposito22 <gius.espo22@gmail.com>
2025-09-15 16:55:29 +0200

    add method show ingredient

--- src/main/java/org/lessons/java/spring_pizzeria/controller/IngredientController.java
+++ src/main/java/org/lessons/java/spring_pizzeria/controller/IngredientController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -46,6 +47,13 @@ public class IngredientController {
         ingredientRepository.save(ingredient);
 
         return"redirect:/ingredients";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable Integer id, Model model){
+
+        model.addAttribute("ingredient", ingredientRepository.findById(id).get());
+        return"ingredients/show";
     }
 
 }
